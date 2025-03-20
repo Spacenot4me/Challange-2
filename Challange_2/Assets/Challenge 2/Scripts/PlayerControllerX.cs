9ca02872278415b3c629b7b74a2bc3393dfd4595
spacenot4me <melentev022@gmail.com>
2025-03-20 11:58:12 +0500

    First commit
    First commit

---
+++ Challange_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeDelay = 1f;
+    private float lastspawnTime;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& Time.time > lastspawnTime + timeDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastspawnTime = Time.time;
+        }
+    }
+}
